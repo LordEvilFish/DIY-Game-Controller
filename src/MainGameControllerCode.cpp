24e01e96bac52dbd80d17e5201440ae66a03be78
LordEvilFish <EvilFishMicrosoft@gmail.com>
2022-06-26 19:09:24 +0700

    newest update

--- src/MainGameControllerCode.cpp
+++ src/MainGameControllerCode.cpp
@@ -17,13 +17,15 @@ const int VRx2 = A2; // Connect to Analog Pin 2
 const int VRy2 = A3; // Connect to Analog Pin 3
 
 
-// parameters for reading the joystick:
+const int mouseButton = 4;    // input pin for the mouse pushButton
+
+bool mouseIsActive = true;    // whether or not to control the mouse
 int range = 12;               // output range of X or Y movement
 int responseDelay = 5;        // response delay of the mouse, in ms
 int threshold = range / 4;    // resting threshold
 int center = range / 2;       // resting position value
 
-bool mouseIsActive = true;    // whether or not to control the mouse
+
 
 
 /*
@@ -67,7 +69,6 @@ void Joy1() {
   }
 
 }
-
 int readAxis(int thisAxis) {
   // read the analog input:
   int reading = analogRead(thisAxis);
@@ -86,16 +87,33 @@ int readAxis(int thisAxis) {
   return distance;
 }
 
+  // return the distance for this axis:
+
+
 void Joy2() {
-    // read and scale the two axes:
+
+
+  // read and scale the two axes:
   int xReading = readAxis(A2);
   int yReading = readAxis(A3);
-  if (mouseIsActive) {
-    Mouse.move(xReading, yReading, 0);
 
-    delay(responseDelay);
-    
+  // if the mouse control state is active, move the mouse:
+
+  Mouse.move(xReading, yReading, 0);
+  
+  // read the mouse button and click or not click:
+  // if the mouse button is pressed:
+
+  // else the mouse button is not pressed:
+  if (digitalRead(mouseButton) == LOW) {
+    Serial.print("Clicked");
+    Mouse.click(MOUSE_LEFT);
+    delay(100);
   }
+  
+  
+
+  delay(responseDelay);
 
 }
 
@@ -127,6 +145,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(16, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
   Keyboard.begin();
   Mouse.begin();
 
@@ -140,6 +159,5 @@ void loop() {
   Joy2();
   RightTrigger();
   LeftTrigger();
-  
 
 }
